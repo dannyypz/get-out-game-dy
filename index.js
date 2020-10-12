027ae5911589d7aa63d1ed72218352357ad9148f
Daniel_Yepez <dannyypz@gmail.com>
2020-10-12 00:18:01 -0400

    second commit with some changes

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const prompt = require('readline-sync')
 
 let playerAnswer = prompt.question('You have found yourself in a room with no obvious exits.\nUse the controls to navigate out of here.\nHit 1 to play ESCAPEROOM ');
 
-
-
+const playAgain = (play) =>{
+  console.log("lets play again!")
+  let startOver = prompt.question("Hit 2 to start");
+  if (startOver === "2"){
+    userChoice2()
+  }
+}
 const userChoice =(answer)=> {
   if (answer === "1"){
     userChoice2()
   }
 
 }
-
 const userChoice2= ()=>{
   console.log("Awesome!\nyou'll do great! now look for the coding book and pull on it, hit 2 when you do");
   let playerAnswer1 = prompt.question("Quick! hit the number 2")
@@ -41,7 +45,10 @@ const userChoice5= () => {
 }
 const userChoice6= () => {
   console.log("Use these keys to open the secret door behind the book case and escape!")
-  let playerAnswer5 = prompt.question("You made it out!!")
-  if (playerAnswer5 === "6"){
-    userChoice7()
+  let playerAnswer5 = prompt.question("You made it out!! hit y to play again- ")
+  if (playerAnswer5 === "y"){
+    userChoice()
+    
   }
+  playAgain()
+}
